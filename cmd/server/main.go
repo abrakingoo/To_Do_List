fe3f9b1621d43625749c92f924697661a72edbc5
abrakingoo <maikoabraham68@gmail.com>
2024-07-14 18:12:01 +0300

    feat: added the routez to handle the api crud operations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,25 +1,39 @@
 package main
 
-import(
-	"net/http"
+import (
 	"log"
+	"net/http"
+	"todoapp/cmd/api"
 	"todoapp/routers"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		url := r.URL.Path
 
-		if url == "/" && r.Method == http.MethodGet{
-			routers.Homehandler(w, r)
+		switch url {
+		case "/":
+			if r.Method == http.MethodPost {
+				api.PostHandler(w, r)
+			} else if r.Method == http.MethodPut {
+				api.EditHandler(w, r)
+			} else if r.Method == http.MethodDelete {
+				api.DeleteHandler(w, r)
+			} else {
+				routers.Homehandler(w, r)
+			}
+		default:
+			http.NotFound(w, r)
 		}
 	})
-	go func(){
+	go func() {
 		if err := http.ListenAndServe(":8080", nil); err != nil {
 			log.Fatal("Error Starting The Server >> ", err)
 		}
 	}()
 	log.Println("Server running on http://localhost:8080")
-	select{}
+	select {}
 }
