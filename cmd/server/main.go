a4861fe4349bca4294de937ec636bbc918ba139e
abrakingoo <maikoabraham68@gmail.com>
2024-07-14 10:19:54 +0300

    feat: added the route handler and route for serving the home page

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,18 @@ package main
 import(
 	"net/http"
 	"log"
+	"todoapp/routers"
 )
 
 func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		url := r.URL.Path
+
+		if url == "/" && r.Method == http.MethodGet{
+			routers.Homehandler(w, r)
+		}
+	})
 	go func(){
 		if err := http.ListenAndServe(":8080", nil); err != nil {
 			log.Fatal("Error Starting The Server >> ", err)
