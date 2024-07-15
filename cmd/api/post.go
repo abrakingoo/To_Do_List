fd7d83ab1e0c723bc5a5dbc9eba22f57f8461e6f
abrakingoo <maikoabraham68@gmail.com>
2024-07-15 08:07:46 +0300

    refactor: changed the code from a dummy response to handling of a real post request functionality

--- cmd/api/post.go
+++ cmd/api/post.go
@@ -2,8 +2,48 @@ package api
 
 import(
 	"net/http"
+	"log"
+	"time"
+	"todoapp/utils"
+	"fmt"
 )
 
+type Task struct {
+    ID string
+    Data string
+    Status     bool
+    Created_at time.Time
+}
+
 func PostHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Post reached"))
+	if r.Method != http.MethodPost {
+		log.Fatal("Unacceptable Method Call >>> ")
+	}
+
+	r.ParseForm()
+	data := r.FormValue("todo")
+
+	newTask := Task{
+		ID:utils.GetId(),
+		Data: data,
+		Status: false,
+		Created_at: time.Now(),
+
+	}
+
+	tasks := utils.LoadTasks()
+	var Tasks []Task
+
+	for i := 0; i < len(tasks); i++ {
+		temp := Task{
+			ID: tasks[i].ID,
+			Data:tasks[i].Data,
+			Status:tasks[i].Status,
+			Created_at:tasks[i].Created_at,
+		}
+
+		Tasks = append(Tasks, temp)
+	}
+	Tasks = append(Tasks, newTask)
+	fmt.Fprint(w, Tasks)
 }
